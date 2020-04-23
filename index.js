63e2656aeb7ff82efe25919fb5d758d468d1a5d4
amit <gautamamits95@gmail.com>
2020-04-23 08:07:11 +0530

    express app working

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var request = require('superagent')
 var app = express();
 var fs = require('fs');
 const readline = require('readline');
+var cors = require('cors')
+app.use(cors())
+
+
+const data = fs.readFileSync('star-wars.txt', 'utf8');
 
 async function chuck(req, res) {
     try {
@@ -40,26 +45,30 @@ app.get('/chuck', chuck)
 // }, 100)
 
 app.get('/counter', function(req, res){
-    let id = 0;
-    const filename='star-wars.txt'
-
-    var data = fs.readFileSync(filename, 'utf8');
-    var lines = data.split("\n");
-    
-    async function myFunction() {
-        if(id === lines.length - 1){
-            res.end()
-            return
-        }
-        
-        res.write(`id: ${id}\n`)
-        res.write(`data: ${lines[id]} \n\n`)
-        id += 1;
-        var rand = Math.round(Math.random() * (1000 - 500)) + 500; // generate new time (between 3sec and 500"s)
-        setTimeout(myFunction, rand);
-    }
-    myFunction();
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-transform',
+        'Connection': 'keep-alive',
+    });
+    const initialIndex = req.headers['last-event-id'] ? Number(req.headers['last-event-id']) + 1 :  0
+    // res.write('\n');
+    myFunction(req, res, initialIndex);
 })
+
+function myFunction(req, res, id) {
+    let lines = data.split("\n");
+    if (id === lines.length - 1) {
+        res.end()
+        return
+    }
+
+    res.write(`id: ${id} \n`);
+    res.write(`data: ${lines[id]} \n\n`);
+    // chrome times out after 2 minutes
+    var rand = Math.round(Math.random() * (60000*4 - 500)) + 500; // generate new time (between 3sec and 500"s)
+    console.log(`${rand / 60000}m`)
+    setTimeout(()=>myFunction(req, res, id+1), rand);
+}
 
 app.use((err, req, res, next) => {
     res.status(500);
